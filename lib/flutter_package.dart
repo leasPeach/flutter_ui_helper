b0508778383a3146ed0599c358f1decfd03ed957
liaogt <liaogt123@qq.com>
2022-01-12 11:25:53 +0800

    rename

---
+++ lib/flutter_package.dart
@@ -0,0 +1,3 @@
+library flutter_package;
+
+export 'package:flutter_package/flutter_package.dart';
--- lib/flutter_ui_helper.dart
+++
@@ -1,3 +0,0 @@
-library flutter_package;
-
-export 'package:flutter_package/flutter_ui_helper.dart';
